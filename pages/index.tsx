3f5bec3dd4fbe2234bd4bf34378f62120b83c118
alexsharich <alexsharich@gmail.com>
2023-07-19 22:15:21 +0200

    check working or not

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Home: NextPageWithLayout = () => (
         <Image src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
     </PageWrapper>
 );
+//some comment
 
 Home.getLayout = getLayout;
 export default Home;
